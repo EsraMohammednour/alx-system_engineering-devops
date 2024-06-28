1896deca28747053786c51c81f227376b15b431f
esraa <esraa2023112@gmail.com>
2024-06-28 22:45:47 +0300

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-package { 'werkzeug':
+package { 'Werkzeug':
   ensure  => '2.1.1',
   provider => 'pip3',
 }
