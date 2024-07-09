d86ba47a03180bf39683eddac643471280b9f82d
esraa <esraa2023112@gmail.com>
2024-07-09 15:38:52 +0300

    done

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,6 +3,11 @@
 exec { 'http header':
 	command  => "sudo apt-get update;
 	sudo apt-get -y install nginx;
+	touch /var/www/html/index.html;
+	sed -i "server_name _;\n\trewrite ^/reddirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanet;" /etc/nginx/sites_available/default;
+	sed -i "server_name _;\n\terror_page 404 /404.html ;" /etc/nginx/sites_available/default;
+	touch /var/www/html/404.html;
+	echo "Ceci n'est pas une page" > /etc/nginx/html/404.html;
 	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites_available/default
 	sudo service nginx restart',
 	provider => shell,
